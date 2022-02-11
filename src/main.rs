4269b64c62dddaaf26e78ec3a76b2d07c78833ac
toast <smol@toast.cyou>
2022-02-10 19:52:03 -0500

    basic groundwork for containers added

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args[1].as_str() {
-        "run" => run(),
+        "run" => run(&args[2]),
         _ => println!("Try invoking a command like 'run'{}", args[1])
     }
 }
 
-fn run() {
-    println!("Running!")
+fn run(command: &String) {
+    println!("Running {}!", &command);
+    std::process::Command::new(command)
+        .output()
+        .expect("Could not launch program");
 }
